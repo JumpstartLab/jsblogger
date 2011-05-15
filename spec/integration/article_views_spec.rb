1e9f6573d6413efc05d5a835ec5941ce0a835f6f
Jeff Casimir <jeff@casimircreative.com>
2011-05-14 20:14:02 -0400

    More tweaks for I do section of testing tutorial

--- spec/integration/article_views_spec.rb
+++ spec/integration/article_views_spec.rb
@@ -2,5 +2,26 @@ require 'spec_helper'
 require 'capybara/rspec'
 
 describe "the articles interface" do
+  before(:all) do
+    @articles = []
+    3.times{ @articles << Fabricate(:article) }
+  end
   
+  describe "on the index page" do
+    before(:each) do
+      visit articles_path
+    end
+    
+    it "should list the article titles" do
+      @articles.each do |article|
+        page.should have_content(article.title)
+      end
+    end
+
+    it "should have links for the articles" do
+      @articles.each do |article|
+        page.should have_link(article.title, :href => article_path(article))
+      end
+    end    
+  end
 end
